a5c2f5f4658e42258157ab011e521974fb54760a
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-05-11 12:07:00 -0500

    feat: :card_file_box: Backend of the query Now Playing using the API

--- js/app.js
+++ js/app.js
@@ -5,7 +5,14 @@ import { trackList } from "./modules/trackList.js"
 import { discoverNewAlbums } from "./modules/albums.js"
 
 
-nowPlaying()
-trackList()
-discoverNewAlbums()
+const searchBoxes = document.querySelectorAll('.searchBox')
+let [search1, search2, search3] = searchBoxes
+
+
+search2.addEventListener('keyup', async function(event){
+    if(event.key== 'Enter'){
+        let data = await nowPlaying(event.target.value)
+    }
+})
+
 
--- js/modules/nowPlaying.js
+++ js/modules/nowPlaying.js
@@ -1,8 +1,28 @@
 export const nowPlaying = async (query) =>{
 
-    let res = await fetch("http://localhost:5503/tracks")
-    let data = await res.json()
-    let song = data.items[0].data.uri
-    console.log(song)
+    // let res = await fetch("http://localhost:5503/tracks")
+    // let data = await res.json()
+    // let song = data.items[0].data.uri
+    // console.log(song)
+
+    const url = `https://spotify23.p.rapidapi.com/search/?q=${query}&type=tracks&offset=0&limit=1&numberOfTopResults=0`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '6b6bcb8cd3mshc3b688124c1c5cep128b1ejsn2eccd5e02437',
+            'x-rapidapi-host': 'spotify23.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        let song = result.tracks.items[0].data.uri
+
+        console.log(song);
+    } catch (error) {
+        console.error(error);
+    }
 
 }
