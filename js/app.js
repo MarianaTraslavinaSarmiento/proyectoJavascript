ccc16156495eeb2a8b22443a4ed50f34045dd0f8
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-05-18 16:42:45 -0500

    feat: :sparkles: Generar canciones y albums random

--- js/app.js
+++ js/app.js
@@ -63,3 +63,62 @@ search3.addEventListener('keyup', async function(event){
     }
 })
 
+function generateRandom(num) {
+
+    
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let result = "";
+    for (let i = 0; i < num; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  
+    return result;
+}
+
+
+async function firstMusic() {
+
+    let data = await nowPlaying(generateRandom(4))
+
+    let updateSong = document.createElement('my-frame')
+    let updateSongsBox = document.querySelector('.now__playing')
+    updateSong.setAttribute("uri", data)
+    updateSongsBox.appendChild(updateSong)
+
+
+    let dataUris = await discoverNewAlbums(generateRandom(4))
+    let updateAlbumBox = document.querySelector('.box__album__list')
+    updateAlbumBox.innerHTML = ``
+    for (let idAlbum of dataUris){
+
+        setTimeout(function() {
+
+            let albumComponent = document.createElement("my-songs-playlist")
+            albumComponent.setAttribute ("uri", idAlbum)
+            updateAlbumBox.append(albumComponent)
+
+        },500)
+
+    }
+
+    let dataUrisTracks = await trackList(generateRandom(4))
+
+    let updateTrackBox = document.querySelector('.box__tracks__list')
+    updateTrackBox.innerHTML = ``
+    for (let idTrack of dataUrisTracks){
+
+        setTimeout(function() {
+            let trackComponent = document.createElement("my-songs-playlist")
+            trackComponent.setAttribute ("uri", idTrack)
+            updateTrackBox.append(trackComponent)
+        },500)
+    }
+
+
+
+}
+
+await firstMusic()
+
